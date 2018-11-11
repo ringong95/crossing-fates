e33181a2fe8d665b1f6fb389d204f66e2b054d54
Ringo Ng <Ringong95@gmail.com>
2018-11-11 12:56:47 -0800

    Added Mxins for card height.

--- src/components/MatchCard/styles.js
+++ src/components/MatchCard/styles.js
@@ -1,9 +1,12 @@
 import styled from 'styled-components'
-
+import Mixins from '../../lib/Mixins';
 
 export const CardContainer = styled.div`
   width:100%
   display:flex
+  height: ${Mixins.cardHeight};
+  border-top:    1px solid  black;
+  border-bottom: 3px solid black;
 `
 
 export const MatchInfoContainer = styled.div`
