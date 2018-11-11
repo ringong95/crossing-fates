2ff85921ba57e989c495cefb1c5dc73aac89a959
Ringo Ng <Ringong95@gmail.com>
2018-11-11 12:56:18 -0800

    Added more Fake matches.

--- src/lib/fakematches.js
+++ src/lib/fakematches.js
@@ -11,7 +11,77 @@ export default [
         p2:{
             name:'George the cat',
             charOne: 'TAG_NI',
-            charTwo: 'TAG_KA'
+            charTwo: 'TAG_TG'
+        }
+    },{
+        date:1541278741,
+        location: 'Ceootaku 2035',
+        source: 'https://www.youtube.com/user/Moopoke',
+        p1:{
+            name:'Fat daddy Jive',
+            charOne: 'TAG_NO',
+            charTwo: 'TAG_RG'
+        },
+        p2:{
+            name:'George the cat',
+            charOne: 'TAG_MI',
+            charTwo: 'TAG_YZ'
+        }
+    },{
+        date:1541278741,
+        location: 'Ceootaku 2035',
+        source: 'https://www.youtube.com/user/Moopoke',
+        p1:{
+            name:'Fat daddy Jive',
+            charOne: 'TAG_JI',
+            charTwo: 'TAG_RC'
+        },
+        p2:{
+            name:'George the cat',
+            charOne: 'TAG_HZ',
+            charTwo: 'TAG_NU'
+        }
+    },{
+        date:1541278741,
+        location: 'Ceootaku 2035',
+        source: 'https://www.youtube.com/user/Moopoke',
+        p1:{
+            name:'Fat daddy Jive',
+            charOne: 'TAG_AG',
+            charTwo: 'TAG_NO'
+        },
+        p2:{
+            name:'George the cat',
+            charOne: 'TAG_MI',
+            charTwo: 'TAG_YZ'
+        }
+    },{
+        date:1541278741,
+        location: 'Ceootaku 2035',
+        source: 'https://www.youtube.com/user/Moopoke',
+        p1:{
+            name:'Fat daddy Jive',
+            charOne: 'TAG_BB',
+            charTwo: 'TAG_RR'
+        },
+        p2:{
+            name:'George the cat',
+            charOne: 'TAG_WD',
+            charTwo: 'TAG_VA'
+        }
+    },{
+        date:1541278741,
+        location: 'Ceootaku 2035',
+        source: 'https://www.youtube.com/user/Moopoke',
+        p1:{
+            name:'Fat daddy Jive',
+            charOne: 'TAG_CH',
+            charTwo: 'TAG_MK'
+        },
+        p2:{
+            name:'George the cat',
+            charOne: 'TAG_AZ',
+            charTwo: 'TAG_ES'
         }
     }
 ]
