0def6d3b62e6b2c3c4c3a256c237b90c76145c4a
Ringo Ng <Ringong95@gmail.com>
2018-11-25 15:05:24 -0800

    Updates

---
+++ precache-manifest.c93e15c1d8e50e835ba5cf5750575802.js
@@ -0,0 +1,182 @@
+self.__precacheManifest = [
+  {
+    "revision": "f5cf012ec83ead3fa33e673b138afd37",
+    "url": "/crossing-fates/static/media/TAG_MI.f5cf012e.png"
+  },
+  {
+    "revision": "6e59c06baae70d04a079",
+    "url": "/crossing-fates/static/css/main.8cb47494.chunk.css"
+  },
+  {
+    "revision": "f5d8e1319d19954de946",
+    "url": "/crossing-fates/static/js/1.f5d8e131.chunk.js"
+  },
+  {
+    "revision": "f53607dd666105b02ff5",
+    "url": "/crossing-fates/static/js/runtime~main.f53607dd.js"
+  },
+  {
+    "revision": "abc3e79557ae03ce404c54879736879c",
+    "url": "/crossing-fates/static/media/TAG_AG.abc3e795.png"
+  },
+  {
+    "revision": "f6b5ce6d327e5112e781ccf5f7cbc177",
+    "url": "/crossing-fates/static/media/TAG_AK.f6b5ce6d.png"
+  },
+  {
+    "revision": "725f5fd089de0db64b5063e703dd3652",
+    "url": "/crossing-fates/static/media/TAG_AZ.725f5fd0.png"
+  },
+  {
+    "revision": "733dc4484c81b1499bbdaec29c88df29",
+    "url": "/crossing-fates/static/media/TAG_BB.733dc448.png"
+  },
+  {
+    "revision": "431c6210e8f31e0dd14418e6b02befe1",
+    "url": "/crossing-fates/static/media/TAG_CH.431c6210.png"
+  },
+  {
+    "revision": "20bc4ded542f384d847f58ec51517c00",
+    "url": "/crossing-fates/static/media/TAG_CR.20bc4ded.png"
+  },
+  {
+    "revision": "e0637e8d22be87dcb84b687e18aa4216",
+    "url": "/crossing-fates/static/media/TAG_ES.e0637e8d.png"
+  },
+  {
+    "revision": "8992662d2f4d11eb5de13c63fed73a9c",
+    "url": "/crossing-fates/static/media/TAG_GD.8992662d.png"
+  },
+  {
+    "revision": "4dfd3d753fcec88f9537124fef3e9bff",
+    "url": "/crossing-fates/static/media/TAG_HK.4dfd3d75.png"
+  },
+  {
+    "revision": "0c10b3fc1e83f30dedc144f9c57d7220",
+    "url": "/crossing-fates/static/media/TAG_HY.0c10b3fc.png"
+  },
+  {
+    "revision": "d38ff452665ed4ed8953a5bb9a931754",
+    "url": "/crossing-fates/static/media/TAG_HZ.d38ff452.png"
+  },
+  {
+    "revision": "198197dfb009a1362bac623fba735554",
+    "url": "/crossing-fates/static/media/TAG_IZ.198197df.png"
+  },
+  {
+    "revision": "86316cd2fe3f5effd30c257928f28ec2",
+    "url": "/crossing-fates/static/media/TAG_JI.86316cd2.png"
+  },
+  {
+    "revision": "baa25a24b78f020b44f1f9fb735d5404",
+    "url": "/crossing-fates/static/media/TAG_JU.baa25a24.png"
+  },
+  {
+    "revision": "942aff4afbcabdccb75e05c6b677a330",
+    "url": "/crossing-fates/static/media/TAG_KA.942aff4a.png"
+  },
+  {
+    "revision": "db432ed2120319cd6a389412128033af",
+    "url": "/crossing-fates/static/media/TAG_LB.db432ed2.png"
+  },
+  {
+    "revision": "48183284b9492ceca862276dcb9a2ea1",
+    "url": "/crossing-fates/static/media/TAG_LN.48183284.png"
+  },
+  {
+    "revision": "c4a943cdef4bd59ac19fff34ee7cb625",
+    "url": "/crossing-fates/static/media/TAG_MA.c4a943cd.png"
+  },
+  {
+    "revision": "6e59c06baae70d04a079",
+    "url": "/crossing-fates/static/js/main.6e59c06b.chunk.js"
+  },
+  {
+    "revision": "3318fa4d5a636a6fde9ad0fc761516f0",
+    "url": "/crossing-fates/static/media/TAG_MK.3318fa4d.png"
+  },
+  {
+    "revision": "f1a04d8539a521981c3f85439fb1a617",
+    "url": "/crossing-fates/static/media/TAG_MR.f1a04d85.png"
+  },
+  {
+    "revision": "2e061625e30fa993faa92fb7c98fec6d",
+    "url": "/crossing-fates/static/media/TAG_MS.2e061625.png"
+  },
+  {
+    "revision": "dd45c6b5a51734bc675d611ec36f4201",
+    "url": "/crossing-fates/static/media/TAG_NA.dd45c6b5.png"
+  },
+  {
+    "revision": "d5794c4d8747c9ff274da72a7b9b7158",
+    "url": "/crossing-fates/static/media/TAG_NI.d5794c4d.png"
+  },
+  {
+    "revision": "6661c6f5f36d65d831f55530cf5267a8",
+    "url": "/crossing-fates/static/media/TAG_NO.6661c6f5.png"
+  },
+  {
+    "revision": "6bcde8560232b84cb6de31ab667235c6",
+    "url": "/crossing-fates/static/media/TAG_NU.6bcde856.png"
+  },
+  {
+    "revision": "2dc01fc102e5f7bf0ea87d8a42b64cee",
+    "url": "/crossing-fates/static/media/TAG_OR.2dc01fc1.png"
+  },
+  {
+    "revision": "c21fdfb732761e623932fbfb22190d28",
+    "url": "/crossing-fates/static/media/TAG_PL.c21fdfb7.png"
+  },
+  {
+    "revision": "6a2eb726c1104ac65dbbf683c5153756",
+    "url": "/crossing-fates/static/media/TAG_RC.6a2eb726.png"
+  },
+  {
+    "revision": "2c84ea316cbe3c81ad32d23d4c417f32",
+    "url": "/crossing-fates/static/media/TAG_RG.2c84ea31.png"
+  },
+  {
+    "revision": "f2958f6e5ae60a26e03d9dc7f9d1cf0e",
+    "url": "/crossing-fates/static/media/TAG_RR.f2958f6e.png"
+  },
+  {
+    "revision": "69c4422d8aabe94fee9d907c9779a4be",
+    "url": "/crossing-fates/static/media/TAG_TG.69c4422d.png"
+  },
+  {
+    "revision": "2cbe3e7115dccf1ea3b9f70b0d62d7b8",
+    "url": "/crossing-fates/static/media/TAG_VA.2cbe3e71.png"
+  },
+  {
+    "revision": "5e1eb5a77550a2c9683eb82f8cd761a5",
+    "url": "/crossing-fates/static/media/TAG_WD.5e1eb5a7.png"
+  },
+  {
+    "revision": "6f576db8938d8d9cf9af96113bb5cab9",
+    "url": "/crossing-fates/static/media/TAG_WS.6f576db8.png"
+  },
+  {
+    "revision": "c9c2c7b01ff08fd50bbaa074988dfefc",
+    "url": "/crossing-fates/static/media/TAG_YK.c9c2c7b0.png"
+  },
+  {
+    "revision": "f548a9ec662b401b7c62d72b76139d19",
+    "url": "/crossing-fates/static/media/TAG_YO.f548a9ec.png"
+  },
+  {
+    "revision": "3dc82e6a1325400d848c8dd10dba536f",
+    "url": "/crossing-fates/static/media/TAG_YU.3dc82e6a.png"
+  },
+  {
+    "revision": "e505d9a85979228b00a762b41ffa35b3",
+    "url": "/crossing-fates/static/media/TAG_YX.e505d9a8.png"
+  },
+  {
+    "revision": "125ce5f18fc583ad122ba1ed8cccc515",
+    "url": "/crossing-fates/static/media/TAG_YZ.125ce5f1.png"
+  },
+  {
+    "revision": "416b8e70fb993fe2ff4a1cf2fe2cd90e",
+    "url": "/crossing-fates/index.html"
+  }
+];
